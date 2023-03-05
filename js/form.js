d7b9d4e0e80dc63c14427b952148ea2b94293eca
amirazak11 <amirazak1112018@gmail.com>
2023-03-05 17:10:01 +0200

    edited mobile-size

--- js/form.js
+++ js/form.js
@@ -1,3 +1,6 @@
+const nameInput = document.querySelector(".name-input");
+const emailInput = document.querySelector(".email-input");
+const phoneInput = document.querySelector(".phone-input");
 const messageInput = document.querySelector(".message-input")
 const sendBtn = document.querySelector(".send-btn")
 const sendMessageToEmail = () => {
@@ -7,7 +10,8 @@ const sendMessageToEmail = () => {
         phone: phoneInput.value,
         message: messageInput.value
     }
-    fetch('https://formsubmit.co/ajax/drmedia34@gmail.com', {
+
+    fetch('https://formsubmit.co/ajax/amirazak1032000@gmail.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
